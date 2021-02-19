8a9bb5f296f1a0232e6df85bdc049f419a4586cd
arsdaniel <arsdaniel63@gmail.com>
2021-02-19 19:44:29 +0700

    foto

--- resources/views/livewire/santri/upload-doc.blade.php
+++ resources/views/livewire/santri/upload-doc.blade.php
@@ -1,13 +1,68 @@
-<div>
-    <form wire:submit.prevent="save">
-        <input type="file" wire:model="kk">
-    
-        @error('kk') <span class="error">{{ $message }}</span> @enderror
-        @if ($kk)
-        Photo Preview:
-        <img src="{{ $kk->temporaryUrl() }}">
-        @endif
-    
-        <button type="submit">Save Photo</button>
-    </form>
+<div class="panel">
+    <div class="col-md-12">
+	    <div class="panel-heading">
+    		<h2 class="panel-title">Lengkapi Biodata Anda </h2>
+	    </div>
+    </div>
+
+    <div class="panel-body">
+        <form wire:submit.prevent="save" class="was-validated">
+            <div class="form-row">
+                <div class="form-group col-md-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="validatedCustomFile">Upload KK</label>
+                        <input type="file" class="custom-file-input" wire:model="kk" id="validatedCustomFile" required>
+                        
+                        @error('kk') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                        @if ($kk)
+                        <img src="{{ $kk->temporaryUrl() }}" class="img-thumbnail" width= "50%" alt="Responsive image" >
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="validatedCustomFile">Upload Ijazah</label>
+                        <input type="file" class="custom-file-input" wire:model="kk" id="validatedCustomFile" required>
+                        
+                        @error('kk') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                        @if ($kk)
+                        <img src="{{ $kk->temporaryUrl() }}" class="img-thumbnail" width= "50%" alt="Responsive image" >
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="validatedCustomFile">Upload KTP</label>
+                        <input type="file" class="custom-file-input" wire:model="kk" id="validatedCustomFile" required>
+                        
+                        @error('kk') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                        @if ($kk)
+                        <img src="{{ $kk->temporaryUrl() }}" class="img-thumbnail" width= "50%" alt="Responsive image" >
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="validatedCustomFile">Upload KTP</label>
+                        <input type="file" class="custom-file-input" wire:model="kk" id="validatedCustomFile" required>
+                        
+                        @error('kk') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                        @if ($kk)
+                        <img src="{{ $kk->temporaryUrl() }}" class="img-thumbnail" width= "50%" alt="Responsive image" >
+                        @endif
+                    </div>
+                </div>
+
+
+                <div class="form-group col-md-12">
+                    <button type="submit" class="btn btn-primary">Save Photo</button>
+                </div>
+            </div>
+        
+           
+        </form>
+    </div>
 </div>
