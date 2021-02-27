3ce68b12fc67dcf358fb9ac329823920bcfb94cb
arsdaniel <arsdaniel63@gmail.com>
2021-02-27 18:20:49 +0700

    doc

--- resources/views/livewire/santri/upload-doc.blade.php
+++ resources/views/livewire/santri/upload-doc.blade.php
@@ -18,65 +18,39 @@
                 <h4>Unggah Dokumen</h4>
               </div>
               <div class="card-body">
-                <form wire:submit.prevent="save" >
-                    <div class="form-group row mb-4">
-                        <label class="col-form-label  col-12 col-md-3 col-lg-8">Unggah hasil scan KK anda. maksimal 1000kb</label>
-                        <div class="col-sm-12 col-md-4">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" wire:model="kk" id="kk">
-                                <div wire:loading wire:target="ijazah" class="text-sm text-gray-500 italic">Uploading...</div>
-                                <label class="custom-file-label" for="customFile">Pilih Berkas</label>
-                                @error('kk') <div class="invalid-feedback">{{ $message }}</div>@enderror
-                              </div>
-                        </div>
-                    </div>
-
-                  
-
-                    <div class="form-group row mb-4">
-                        <label class="col-form-label  col-12 col-md-3 col-lg-8">KK Unggah hasil scan KK anda. maksimal 1000kb</label>
-                        <div class="col-sm-12 col-md-4">
-                        <input type="file" class="custom-file" wire:model="ijazah" id="ijazah" required>
-                            <div wire:loading wire:target="ijazah" class="text-sm text-gray-500 italic">Uploading...</div>
-                            @error('ijazah') <div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-4">
-                        <label class="col-form-label  col-12 col-md-3 col-lg-8">KK Unggah hasil scan KK anda. maksimal 1000kb</label>
-                        <div class="col-sm-12 col-md-4">
-                        <input type="file" class="custom-file" wire:model="spSantri" id="spSantri" required>
-                            <div wire:loading wire:target="spSantri" class="text-sm text-gray-500 italic">Uploading...</div>
-                            @error('spSantri') <div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-4">
-                        <label class="col-form-label  col-12 col-md-3 col-lg-8">KK Unggah hasil scan KK anda. maksimal 1000kb</label>
-                        <div class="col-sm-12 col-md-4">
-                        <input type="file" class="custom-file" wire:model="spWsantri" id="spWsantri" required>
-                            <div wire:loading wire:target="spWsantri" class="text-sm text-gray-500 italic">Uploading...</div>
-                            @error('spWsantri') <div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-4">
-                        <label class="col-form-label  col-12 col-md-3 col-lg-8">KK Unggah hasil scan KK anda. maksimal 1000kb</label>
-                        <div class="col-sm-12 col-md-4">
-                        <input type="file" class="custom-file" wire:model="spiQabul" id="spiQabul" required>
-                            <div wire:loading wire:target="spiQabul" class="text-sm text-gray-500 italic">Uploading...</div>
-                            @error('spiQabul') <div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
-
+                <form wire:submit.prevent="save"  class="was-validated" >
+                  <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Kartu Keluarga
+                      <span class="badge badge-primary badge-pill"> Pilih File
+                        <input type="file" class="custom-file-input" wire:model="kk" id="kk" required>
+                      </span>
+                      <div wire:loading wire:target="kk" class="text-sm text-gray-500 italic">Uploading...</div>
+                        @error('kk') <div class="error">{{ $message }}</div>@enderror
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Ijazah
+                      <span class="badge badge-primary badge-pill">Unduh</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Surat Pernyataan Santri
+                      <span class="badge badge-primary badge-pill">Unduh</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Surat Pernyataan Wali Santri
+                      <span class="badge badge-primary badge-pill">Unduh</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Surat Ijab Qabul
+                      <span class="badge badge-primary badge-pill">Unduh</span>
+                    </li>
                     
-
-
-                    <div class="form-group col-md-12">
+                  </ul>
+                  <div class="form-group col-md-12">
                                                
-                        <button type="submit" class="btn btn-lg btn-success ">Simpan Berkas</button>
-                     
-                    </div>
+                    <button type="submit" class="btn btn-primary ">Simpan Berkas</button>
+                 
+                </div>
                 </form>
               </div>
             </div>
@@ -108,3 +82,11 @@
       </div>     
     </div>
   </section>
+
+  <script>
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").on("change", function() {
+        var kk = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(kk);
+        });
+    </script>
