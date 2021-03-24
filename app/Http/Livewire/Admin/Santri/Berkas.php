919cd672b3eb5873385cd39a706fe92c0f5541af
arsdaniel <arsdaniel63@gmail.com>
2021-03-24 16:00:45 +0700

    dd

--- app/Http/Livewire/Admin/Santri/Berkas.php
+++ app/Http/Livewire/Admin/Santri/Berkas.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Admin\Santri;
 
+use App\Models\Biayapendaftaran;
 use App\Models\User;
 use Livewire\Component;
 use Spatie\MediaLibrary\Support\MediaStream;
@@ -9,18 +10,20 @@ use Spatie\MediaLibrary\Support\MediaStream;
 class Berkas extends Component
 {
 
-    public $user_id, $name, $konfirmasi;
+    public $user_id, $name, $konfirmasi, $statusDaftar;
     public $doc=[];
 
     public function mount($user){
         $this->user_id = $user->id; 
         $this->name = $user->name; 
         $this->doc = $user->Doc;
+        $this->statusDaftar = $user->statusdaftar;
     }
 
 
     public function render()
     {   
+        dd($this->pembayaran());
         return view('livewire.admin.santri.berkas');
     }
 
@@ -36,6 +39,20 @@ class Berkas extends Component
                 'status' => '4',]);
         }
 
+    }
+
+    public function getBayar(){
+        $Biayapendaftaran = Biayapendaftaran::where('tahun_ajaran', '2021')
+                            ->where('sekolah', '1')
+                            ->get();
+        
+        return $Biayapendaftaran;
+
+    }
+
+    public function pembayaran(){
+        $harga_daftar = $this->getBayar();
+        
     }
 
    
